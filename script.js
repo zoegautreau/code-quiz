889358406c311271e42b9679abc779269d65b26b
Zoe Gautreau <gautreauzoe@gmail.com>
2021-03-25 10:46:46 -0300

    added to js file

---
+++ script.js
@@ -0,0 +1,17 @@
+var startButton = document.getElementById("start-button")
+var questionContainerElement = document.getElementById("question-container")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    console.log('Started')
+    //startButton.classList.add
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
